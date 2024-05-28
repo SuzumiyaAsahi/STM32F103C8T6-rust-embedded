66644d215fe625c0e45d63f4f1f3ff4b9408dcb4
SuzumiyaAsahi <784441918@qq.com>
2024-05-28 22:41:12 +0800

    好

---
+++ src/bin/05_homework5.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+                     // use panic_abort as _; // requires nightly
+                     // use panic_itm as _; // logs messages over ITM; requires ITM support
+                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+use rtt_target::{rprint, rprintln, rtt_init_print};
+use stm32f1xx_hal::{adc, pac, prelude::*, timer::Timer};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let dp = pac::Peripherals::take().unwrap();
+    let rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+
+    // 选择时钟6分频
+    let clocks = rcc.cfgr.adcclk(6.MHz()).freeze(&mut flash.acr);
+
+    let mut adc1 = adc::Adc::adc1(dp.ADC1, clocks);
+
+    let mut gpioa = dp.GPIOA.split();
+
+    let mut ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
+
+    loop {
+        let data: u16 = adc1.read(&mut ch0).unwrap();
+        let v = f32::from(data);
+        rprintln!("adc1: {}", v / 4095 * 3.3);
+    }
+    a
+}
---
+++ src/bin/06_homework1.rs
@@ -0,0 +1,47 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+                     // use panic_abort as _; // requires nightly
+                     // use panic_itm as _; // logs messages over ITM; requires ITM support
+                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+use key::my_key;
+use led::my_led;
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+
+#[path = "../key/mod.rs"]
+mod key;
+
+#[path = "../led/mod.rs"]
+mod led;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+    let rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut timer = Timer::syst(cp.SYST, &clocks).delay();
+
+    let (mut led1, mut led2) = my_led::led_init(dp.GPIOA.split());
+
+    let (mut key1, mut key2) = my_key::key_init(dp.GPIOB.split());
+
+    rprintln!("Hello, world!");
+    loop {
+        if key1.is_entered(&mut timer) {
+            led1.led_turn();
+        }
+
+        if key2.is_entered(&mut timer) {
+            led2.led_turn();
+        }
+    }
+}
